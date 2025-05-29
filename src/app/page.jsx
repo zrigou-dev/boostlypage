c08601e2a574b12f4cead05eec0e317543c72765
zrigoudev <zrigoudev@gmail.com>
2025-05-29 23:37:18 +0200

    add icon whatsap and sitemap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Benefits from "@/components/Benefits";
 import HeroSection from "../components/HeroSection";
 import Portfolio from "@/components/Portfolio";
@@ -7,19 +8,27 @@ import Contact from "@/components/Contact";
 import FAQ from "@/components/Faq";
 import Footer from "@/components/Footer";
 import Features from "@/components/Features";
+import { FaWhatsapp } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative">
       <HeroSection />
       <Benefits />
-      <Features/>
+      <Features />
       <Portfolio />
       <Stats />
-      <Pricing/>
-      <Contact/>
-      <FAQ/>
-      <Footer/>
-    </>
+      <Pricing />
+      <Contact />
+      <FAQ />
+      <Footer />
+      <a
+        target="_blank"
+        href="https://wa.me/+212767496526"
+        className="fixed bottom-5 right-5 w-[50px] h-[50px] bg-green-600 z-30 flex justify-center items-center rounded-full"
+      >
+        <FaWhatsapp className="text-2xl text-white" />
+      </a>
+    </div>
   );
 }
