c81506e6b142da429ed089e6a902f724b803283c
zrigou-dev <zrigoudev@gmail.com>
2026-04-11 14:41:58 +0100

    chore: update linting command to use explicit eslint configuration with directory ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    ignores: [".next/*", "node_modules/*", "public/*", "tests/*"],
+  },
+  ...compat.extends("next/core-web-vitals"),
+];
 
 export default eslintConfig;
